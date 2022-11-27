b1e0a91deedbc4bc21f1e131bbd74082d4424ec0
hephzisoft <hephzisoft@gmail.com>
2022-11-27 23:07:11 +0100

    made some changes

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -50,7 +50,7 @@ class _RegisterViewState extends State<RegisterView> {
                     enableSuggestions: false,
                     autocorrect: false,
                     decoration: const InputDecoration(
-                      hintText: "Email Address ",
+                      hintText: "Email  ",
                     ),
                   ),
                   TextField(
