2a6bbd0a45ac778689809d7801750f7ec3342fe8
hephzisoft <hephzisoft@gmail.com>
2022-11-29 22:47:07 +0100

    made some certain changes to the login_view and the register_view

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/views/login_view.dart';
-// import 'package:mynotes/views/register_view.dart';
+import 'package:mynotes/views/register_view.dart';
 
 import 'firebase_options.dart';
 
@@ -14,6 +14,10 @@ void main() {
       primarySwatch: Colors.blueGrey,
     ),
     home: const HomePage(),
+    routes: {
+      '/login/':(context) => const LoginView(),
+       '/register/':(context) => const RegisterView(),
+    },
   ));
 }
 
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -28,49 +28,56 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextField(
-          controller: _email,
-          keyboardType: TextInputType.emailAddress,
-          enableSuggestions: false,
-          autocorrect: false,
-          decoration: const InputDecoration(
-            hintText: "Email Address ",
+    return Scaffold(
+      appBar: AppBar(title: const Text("Login"),),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            keyboardType: TextInputType.emailAddress,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(
+              hintText: "Email Address ",
+            ),
           ),
-        ),
-        TextField(
-          controller: _password,
-          obscureText: true,
-          enableSuggestions: false,
-          autocorrect: false,
-          decoration: const InputDecoration(
-            hintText: "Password ",
+          TextField(
+            controller: _password,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(
+              hintText: "Password ",
+            ),
           ),
-        ),
-        TextButton(
-          onPressed: () async {
-            final email = _email.text;
-            final password = _password.text;
-
-            try {
-              final userCrediential = await FirebaseAuth.instance
-                  .signInWithEmailAndPassword(email: email, password: password);
-              print(userCrediential);
-            } on FirebaseAuthException catch (e) {
-              if (e.code == "user-not-found") {
-                print("User not found");
-              } else if (e.code == "wrong-password") {
-                print("Wrong password ");
+          TextButton(
+            onPressed: () async {
+              final email = _email.text;
+              final password = _password.text;
+    
+              try {
+                final userCrediential = await FirebaseAuth.instance
+                    .signInWithEmailAndPassword(email: email, password: password);
+                print(userCrediential);
+              } on FirebaseAuthException catch (e) {
+                if (e.code == "user-not-found") {
+                  print("User not found");
+                } else if (e.code == "wrong-password") {
+                  print("Wrong password ");
+                }
               }
-            }
-          }, 
-          child: const Text("Login"),
-        ),
-        TextButton(
-            onPressed: () {},
-            child: const Text("Not registered yet? Register here")),
-      ],
+            },
+            child: const Text("Login"),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context)
+                  .pushNamedAndRemoveUntil('/register/', (route) => false);
+            },
+            child: const Text("Not registered yet? Register here"),
+          ),
+        ],
+      ),
     );
   }
 }
