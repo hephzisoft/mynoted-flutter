551354c9e9b69e8626c8e55d966403a49d244abb
hephzisoft <hephzisoft@gmail.com>
2022-11-29 07:27:30 +0100

    added some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:mynotes/views/login_view.dart';
-import 'package:mynotes/views/register_view.dart';
+// import 'package:mynotes/views/login_view.dart';
+// import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_view.dart';
 
 import 'firebase_options.dart';
