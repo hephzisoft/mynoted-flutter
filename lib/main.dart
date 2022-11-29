58193f5098e478534660cef0ea7f2a9968ba1374
hephzisoft <hephzisoft@gmail.com>
2022-11-29 07:08:23 +0100

    added some things while making some changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,15 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/views/login_view.dart';
+import 'package:mynotes/views/register_view.dart';
+import 'package:mynotes/views/verify_view.dart';
 
 import 'firebase_options.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
-    title: 'Flutter Demo',
+    title: 'My notes',
     theme: ThemeData(
       primarySwatch: Colors.blueGrey,
     ),
@@ -38,7 +40,7 @@ class HomePage extends StatelessWidget {
                 print("You are a verified user");
               } else {
                 print("You need to verify your email first");
-                return const VerifyEmailView();
+                return const VerifyView();
               }
               return const Text("Done");
             default:
--- lib/views/verify_view.dart
+++ lib/views/verify_view.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -9,8 +13,42 @@ class VerifyView extends StatefulWidget {
 }
 
 class _VerifyViewState extends State<VerifyView> {
+  final auth = FirebaseAuth.instance;
+  User? user;
+  Timer? timer;
+
+  @override
+  void initState() {
+    user = auth.currentUser;
+    user?.sendEmailVerification();
+
+    timer = Timer.periodic(const Duration(seconds: 5), (timer) {});
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      
+      body: Center(
+        child:  Text("An email has been sent to ${user?.emailVerified}, please verify"),
+      ),
+    );
+  }
+
+  Future<void> checkEmailVerified() async {
+    user = auth.currentUser;
+    await user?.reload();
+    if (user?.emailVerified ?? false) {
+      timer?.cancel();
+      print("Email has been verifed");
+      
+    }
   }
 }
